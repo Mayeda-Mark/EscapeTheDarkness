5e6360ddab6724e473de1bba8309cd161c6f3c9e
Mark Mayeda <markrmayeda@gmail.com>
2020-09-19 06:31:08 -0700

    more traps added

---
+++ Escape The Darkness/Assets/Scripts/SpinningTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningTrap : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0, 0, rotationSpeed));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
